7d04e429a68dfd3a3ebd0dfc8a602d4d5b11cd05
Geetha Pisapati <geetha.pisapati@gmail.com>
2016-08-30 20:29:51 -0500

    Add variable playgroud area

---
+++ variable-playground.js
@@ -0,0 +1,28 @@
+// var person = {
+// 	name: 'Geetha',
+// 	age: 20
+// };
+
+// function updatePerson(obj) {
+// 	// obj = {
+// 	// 	name: 'Geetha',
+// 	// 	age: 25
+// 	// };
+// 	obj.age = 25;
+// }
+
+// updatePerson(person);
+// console.log(person);
+
+//Array Example
+var grades = [15, 37];
+
+function addGrade(gradesArr) {
+	gradesArr.push(55);
+	debugger;
+	//gradesArr = [12, 90, 40];
+	//return gradesArr;
+}
+
+addGrade(grades);
+console.log(grades);
