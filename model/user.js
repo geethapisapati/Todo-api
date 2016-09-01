8dc5d45c9318644083ad542c3978245e62f9c876
Geetha Pisapati <geetha.pisapati@gmail.com>
2016-09-01 13:50:46 -0500

    Convert Email to lowercase

--- model/user.js
+++ model/user.js
@@ -15,5 +15,14 @@ module.exports = function(sequelize, DataTypes) {
 				len: [7, 100]
 			}
 		}
+	}, {
+		hooks: {
+			beforeValidate: function(user, options) {
+				//user.email to lowercase if it is only a string
+				if(typeof user.email === "string") {
+					user.email = user.email.toLowerCase();
+				}
+			}
+		}
 	});
 }
