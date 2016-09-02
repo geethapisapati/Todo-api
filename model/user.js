cea60bd4614fe91e0ff80f79aa74f16de2b996e6
Geetha Pisapati <geetha.pisapati@gmail.com>
2016-09-02 17:33:28 -0500

    Add auth middleware and findByToken Class

--- model/user.js
+++ model/user.js
@@ -28,6 +28,7 @@ module.exports = function(sequelize, DataTypes) {
 			set: function(value) {
 				var salt = bcrypt.genSaltSync(10);
 				var hashedPassword = bcrypt.hashSync(value, salt);
+
 				this.setDataValue('password', value);
 				this.setDataValue('salt', salt);
 				this.setDataValue('password_hash', hashedPassword);
@@ -36,19 +37,20 @@ module.exports = function(sequelize, DataTypes) {
 	}, {
 		hooks: {
 			beforeValidate: function(user, options) {
-				//user.email to lowercase if it is only a string
-				if (typeof user.email === "string") {
+				// user.email
+				if (typeof user.email === 'string') {
 					user.email = user.email.toLowerCase();
 				}
 			}
 		},
 		classMethods: {
 			authenticate: function(body) {
-				return new Promise(function (resolve, reject) {
-					if (typeof body.email !== "string" || typeof body.password !== "string") {
+				return new Promise(function(resolve, reject) {
+					if (typeof body.email !== 'string' || typeof body.password !== 'string') {
 						return reject();
 					}
-					user.findOne({ 
+
+					user.findOne({
 						where: {
 							email: body.email
 						}
@@ -56,10 +58,32 @@ module.exports = function(sequelize, DataTypes) {
 						if (!user || !bcrypt.compareSync(body.password, user.get('password_hash'))) {
 							return reject();
 						}
+
 						resolve(user);
 					}, function(e) {
 						reject();
 					});
+				});
+			},
+			findByToken: function(token) {
+				return new Promise(function(resolve, reject) {
+					try {
+						var decodedJWT = jwt.verify(token, 'qwerty098');
+						var bytes = cryptojs.AES.decrypt(decodedJWT.token, 'abc123!@#!');
+						var tokenData = JSON.parse(bytes.toString(cryptojs.enc.Utf8));
+
+						user.findById(tokenData.id).then(function (user) {
+							if (user) {
+								resolve(user);
+							} else {
+								reject();
+							}
+						}, function (e) {
+							reject();
+						});
+					} catch (e) {
+						reject();
+					}
 				});
 			}
 		},
@@ -68,13 +92,16 @@ module.exports = function(sequelize, DataTypes) {
 				var json = this.toJSON();
 				return _.pick(json, 'id', 'email', 'createdAt', 'updatedAt');
 			},
-			generateToken:  function (type) {
+			generateToken: function(type) {
 				if (!_.isString(type)) {
 					return undefined;
 				}
 
 				try {
-					var stringData = JSON.stringify({id: this.get('id'), type: type});
+					var stringData = JSON.stringify({
+						id: this.get('id'),
+						type: type
+					});
 					var encryptedData = cryptojs.AES.encrypt(stringData, 'abc123!@#!').toString();
 					var token = jwt.sign({
 						token: encryptedData
@@ -88,5 +115,6 @@ module.exports = function(sequelize, DataTypes) {
 			}
 		}
 	});
+
 	return user;
 };
